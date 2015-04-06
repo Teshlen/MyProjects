8a193a77ea487b100d7fe72fb3f7ae4ecac3eea0
Uteshlen Nadesan <tnadesan@gmail.com>
2015-04-06 21:34:47 +0200

    added new sql project

---
+++ SqlLiteExample/SqlLiteExampleAndroid/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using System.Diagnostics;
+
+namespace SqlLiteExampleAndroid
+{
+	[Activity (Label = "SqlLiteExampleAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			System.Diagnostics.Debug.WriteLine ("------ hello");
+		
+			//SqlDatabase.createDatabase ();
+			//var p = SqlDatabase.insertUpdateData (null, "database1.db");
+			//var x = SqlDatabase.findNumberRecords ("database1.db");
+			//System.Diagnostics.Debug.WriteLine (x.Id + " ---- " + x.ToString());
+
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+
+			Button createTableButton = FindViewById<Button> (Resource.Id.button1);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks! {1}", count++, SqlDatabase.blap().FirstOrDefault());
+			};
+
+			createTableButton.Click += delegate {
+				SqlDatabase.createTable();
+				createTableButton.Text = "Table Created";
+
+			};
+		}
+	}
+		
+}
+
+
---
+++ SqlLiteExample/SqlLiteExampleAndroid/SqlDatabase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using SQLite;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SqlLiteExampleAndroid
+{
+	public static class SqlDatabase
+	{
+		public static void createTable()
+		{
+			Person p = new Person{FirstName = "Teshlen", ID = 1, LastName = "Nadesan" };
+			string dbPath = Path.Combine (
+				Environment.GetFolderPath (Environment.SpecialFolder.Personal),
+				"ormdemo.db3");
+			var db = new SQLiteConnection (dbPath);
+			db.CreateTable<Person> ();
+			db.Insert (p);
+		}
+
+		public static TableQuery<Person> blap ()
+		{
+			Person p = new Person{FirstName = "Teshlen", ID = 1, LastName = "Nadesan" };
+			string dbPath = Path.Combine (
+				Environment.GetFolderPath (Environment.SpecialFolder.Personal),
+				"ormdemo.db3");
+			var db = new SQLiteConnection (dbPath);
+			//db.CreateTable<Person> ();
+			//db.Insert (p);
+
+			var y = db.Table<Person> ();
+			Debug.WriteLine("----------" +  y.FirstOrDefault ().FirstName + " " + y.FirstOrDefault().LastName);
+
+			TableQuery<Person> i = from a in y
+					where a.FirstName == "Teshlen"
+				select a ;
+			return i;
+		}
+
+	}
+}
+
